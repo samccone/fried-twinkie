ee1c9910c32129defc65684f1b3487daa2eddc79
Sam Saccone <samccone@gmail.com>
2017-08-12 11:25:41 -0700

    Handle case when there is no found source.
    This was we always return an error string and not undefined.

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ function getFormattedErrorString(
     lineNo: number;
     charNo: number;
   }
-) {
+): string {
   let errorString = "";
 
   if (errorMessage.file) {
@@ -49,7 +49,7 @@ function getFormattedErrorString(
   }
 
   if (source.length === 0) {
-    return;
+    return errorString;
   }
 
   const lineIndicator = `${errorMessage.lineNo}: `;
